ade0e8da454aee51590364f356b70bd6b5731449
gogokgc <gogokgc@gmail.com>
2021-05-13 16:18:56 +0900

    5.3 스프링 시큐리티와 OAuth 2.0 으로 basic 구글로그인 구현 vol.2

--- src/main/java/com/kgc/book/springboot/web/IndexController.java
+++ src/main/java/com/kgc/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kgc.book.springboot.web;
 
+import com.kgc.book.springboot.config.auth.dto.SessionUser;
 import com.kgc.book.springboot.srvice.posts.PostsService;
 import com.kgc.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { //서버 템플릿 엔진에서 사용할수잇는 객체를 저장, 여기서는 postsService.findAllDesc( )로 가져온 결과를 posts로 index.mustache에 전달합니다.
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//      앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
