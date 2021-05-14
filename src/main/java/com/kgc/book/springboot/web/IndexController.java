f9cbdeb38826c18ce3962b6ee572c47c8cfd95ef
gogokgc <gogokgc@gmail.com>
2021-05-14 16:20:17 +0900

    5.3 스프링 시큐리티와 OAuth 2.0 으로 basic 구글로그인 구현 vol.3 어노테이션 기반으로 개선하기

--- src/main/java/com/kgc/book/springboot/web/IndexController.java
+++ src/main/java/com/kgc/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kgc.book.springboot.web;
 
+import com.kgc.book.springboot.config.auth.LoginUser;
 import com.kgc.book.springboot.config.auth.dto.SessionUser;
 import com.kgc.book.springboot.srvice.posts.PostsService;
 import com.kgc.book.springboot.web.dto.PostsResponseDto;
@@ -18,11 +19,11 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
 
-    @GetMapping("/")
-    public String index(Model model) { //서버 템플릿 엔진에서 사용할수잇는 객체를 저장, 여기서는 postsService.findAllDesc( )로 가져온 결과를 posts로 index.mustache에 전달합니다.
+    @GetMapping("/") // 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 되었습니다.
+    public String index(Model model, @LoginUser SessionUser user) { //서버 템플릿 엔진에서 사용할수잇는 객체를 저장, 여기서는 postsService.findAllDesc( )로 가져온 결과를 posts로 index.mustache에 전달합니다.
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 //      앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
 
         if(user != null){
